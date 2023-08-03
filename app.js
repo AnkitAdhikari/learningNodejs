c4bf6606baa85bf79a0feddb0a5ccc0e1192e9fc
AnkitAdhikari <ankitadhikari31512@gmail.com>
2023-08-03 21:01:20 +0545

    Day 3

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("<h2>I am on home page</h2>");
+});
+
+app.get("/about", (req, res) => {
+  res.send("I on about page");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("I am on contact page, nodemon working");
+});
+
 app.listen(3000, function () {
   console.log("Node js project has been started\n");
   // git -rm --cached node_modules
