6d75c02836c531024a3776a60cf6a523048e527a
AnkitAdhikari <ankitadhikari31512@gmail.com>
2023-08-04 20:57:59 +0545

    Day 4(ejs,CSR,SSR)

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.send("<h2>I am on home page</h2>");
+  // res.send("<h2>I am on home page</h2>");
+  res.render("home.ejs", { name: "Ankit", age: 18 });
 });
 
 app.get("/about", (req, res) => {
-  res.send("I on about page");
+  res.render("about.ejs");
 });
 
 app.get("/contact", (req, res) => {
-  res.send("I am on contact page, nodemon working");
+  res.render("contact");
 });
 
 app.listen(3000, function () {
